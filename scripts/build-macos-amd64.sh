ac54aaedfe6c5db3e7b25c5db5061e1a76e2493c
vl <vladchudov636@gmail.com>
2024-09-21 11:25:42 +0500

    windows port

--- scripts/build-macos-amd64.sh
+++ scripts/build-macos-amd64.sh
@@ -4,5 +4,5 @@ git submodule init && git submodule update
 
 brew install sdl2
 
-./waf configure -T debug --disable-warns $* &&
+./waf configure -T release --build-game=tf_port --disable-warns $* &&
 ./waf build
