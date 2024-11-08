721e213a1eda725fdf0b9e3346cc70cc853d5942
vl <vladchudov636@gmail.com>
2024-11-08 16:22:52 +0500

    fix script

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -1,7 +1,7 @@
 export ICON=tf2.png
 export PACKAGE=tf_port
 export APP_NAME="Team Fortress 2 (2008)"
-export EXTRAS="tf_extras/extras_dir.vpk"
+export EXTRAS="tf2_extras/extras_dir.vpk"
 cd srceng-mod-launcher
 git clone https://gitlab.com/LostGamer/android-sdk
 export ANDROID_SDK_HOME=$PWD/android-sdk
